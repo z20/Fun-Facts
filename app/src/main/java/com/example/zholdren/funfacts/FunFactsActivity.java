976b21322dca16ce3e9fe77e509620b5cfd156df
Zach <zholdren20@gmail.com>
2014-10-27 17:23:44 -0400

    added fun facts and displayed facts on button click

--- app/src/main/java/com/example/zholdren/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/zholdren/funfacts/FunFactsActivity.java
@@ -4,6 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 
 public class FunFactsActivity extends Activity {
@@ -12,7 +17,43 @@ public class FunFactsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        // Declare our View variables and assign them the Views from the layout file
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] facts = {
+                        "Ants stretch when they wake up in the morning.",
+                        "Ostriches can run faster than horses.",
+                        "Olympic gold medals are actually made mostly of silver.",
+                        "You are born with 300 bones; by the time you are an adult you will have 206.",
+                        "It takes about 8 minutes for light from the Sun to reach Earth.",
+                        "Some bamboo plants can grow almost a meter in just one day.",
+                        "The state of Florida is bigger than England.",
+                        "Some penguins can leap 2-3 meters out of the water.",
+                        "On average, it takes 66 days to form a new habit.",
+                        "Mammoths still walked the earth when the Great Pyramid was being built." };
+
+                // the button was clicked so update the fact label with a new fact
+                String fact = "";
+                // Randomly select a fact
+                Random randomGenerator = new Random();
+                int randomNumber = randomGenerator.nextInt(facts.length);
+
+
+                fact = facts[randomNumber];
+
+
+                // Update the label with our dynamic fact
+                factLabel.setText(fact);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
+
     }
+
 
 
     @Override

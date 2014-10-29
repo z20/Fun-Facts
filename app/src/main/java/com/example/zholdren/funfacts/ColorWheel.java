05404d57f657fbdb7c145e3dcd62e67ab24cc94e
Zach <zholdren20@gmail.com>
2014-10-28 22:39:27 -0400

    Added background colors for each fact

---
+++ app/src/main/java/com/example/zholdren/funfacts/ColorWheel.java
@@ -0,0 +1,43 @@
+package com.example.zholdren.funfacts;
+
+import android.graphics.Color;
+
+import java.util.Random;
+
+/**
+ * Created by zholdren on 10/28/14.
+ */
+public class ColorWheel {
+
+    // Member variable (properties about the object)
+    public String[] mColors = {
+            "#39add1",
+            "#3079ab",
+            "#c25975",
+            "#e15258",
+            "#f9845b",
+            "#838cc7",
+            "#7d669e",
+            "#53bbb4",
+            "#51b46d",
+            "#e0ab18",
+            "#637a91",
+            "#f092b0",
+            "#b7c0c7"
+    };
+
+    // Method (abilities: things the object can do)
+    public int getColor() {
+
+        String color = "";
+
+        // Randomly select a fact
+        Random randomGenerator = new Random();  // Construct a new Random number generator
+        int randomNumber = randomGenerator.nextInt(mColors.length);
+
+        color = mColors[randomNumber];
+        int colorAsInt = Color.parseColor(color);
+
+        return colorAsInt;
+    }
+}
